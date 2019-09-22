7c72ca28d1f333a5a65e0a497627f14b3ca15584
xiaohu-v587 <55342768+xiaohu-v587@users.noreply.github.com>
2019-09-22 22:56:56 +0800

    Update test.java

--- idop_version/src/test.java
+++ idop_version/src/test.java
@@ -17,7 +17,7 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("118");
+		System.out.println("120");
 		System.out.println(a(3));;
 		
 	}
