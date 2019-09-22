e006328bfe431db0e94c6be888e54e395f60ea91
xiaohu <liuxh@goodcol.com>
2019-09-22 21:47:03 +0800

    test1

--- idop_version/src/Server.java
+++ idop_version/src/Server.java
@@ -7,7 +7,7 @@ import com.goodcol.core.core.Gcds;
  */ 
 public class Server {
 	public static void main(String[] args) {
-		//test222
+		//test222444
 		String webAppDir=Server.class.getResource("/").getPath().replace("/WEB-INF/classes/", "");
 		int port=79; 
 		String content="/";
