764a5ed40f65d2cf3df9f45d87043db770980502
xiaohu <liuxh@goodcol.com>
2019-09-23 22:35:27 +0800

    lll

--- idop_version/src/test.java
+++ idop_version/src/test.java
@@ -17,7 +17,7 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("121");
+		System.out.println("1213333");
 		System.out.println(a(3));;
 		
 	}
