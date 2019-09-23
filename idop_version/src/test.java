ae28c0a06c0af2d999f65f2162bc386eff0bd7cf
xiaohu <liuxh@goodcol.com>
2019-09-23 22:43:06 +0800

    88

--- idop_version/src/test.java
+++ idop_version/src/test.java
@@ -19,7 +19,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("1213333");
 		System.out.println("121111");
-		System.out.println(a(3));
+		System.out.println(a(3));;
 		
 	}
 	
